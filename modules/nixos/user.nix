374286d8a0204d957a345a15201cce70d7c2e026
Dillon Johnson <me@nollidnosnhoj.com>
2025-05-30 20:37:38 -1000

    add ssh

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -10,8 +10,15 @@
     extraGroups = [
       "networkmanager"
       "wheel"
+      "video"
+      "input"
     ];
     shell = pkgs.fish;
+    openssh.authorizedKeys = {
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWqCDUOQLGX3ajsocvg8IoEuAabhq9HYb1WeHSZ1Xtu me@nollidnosnhoj.com"
+      ];
+    };
   };
   nix.settings.allowed-users = [ "${username}" ];
 }
