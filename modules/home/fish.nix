561c11b6c49d3faa951f18c980dea2297e6aad34
Dillon Johnson <me@nollidnosnhoj.com>
2025-06-30 16:06:16 -1000

    remove fast fetch in fish init

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -1,11 +1,6 @@
 {...}: {
   programs.fish = {
     enable = true;
-    interactiveShellInit = "
-      
-      set -g fish_greeting ''
-      fastfetch
-    ";
 
     shellAliases = {
       ll = "ls -la";
