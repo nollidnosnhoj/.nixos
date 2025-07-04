53be8eb3178959adb0b872a4ee4c7a7852041f73
Dillon Johnson <me@nollidnosnhoj.com>
2025-07-04 02:07:46 -1000

    remove iwd

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,21 +1,10 @@
-{pkgs, ...}: {
+{...}: {
   networking = {
     networkmanager = {
       enable = true;
-      wifi.backend = "iwd";
-    };
-    wireless = {
-      iwd = {
-        enable = true;
-        settings = {
-          Settings.AutoConnect = true;
-        };
-      };
+      wifi.scanRandMacAddress = false;
     };
   };
 
   services.openssh.enable = true;
-
-  # Don't wait for network startup
-  systemd.services.NetworkManager-wait-online.serviceConfig.ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
 }
