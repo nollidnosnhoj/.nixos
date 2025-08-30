04f413d268fbf84908afae62ad7ac6578a1f2347
Dillon Johnson <me@nollidnosnhoj.com>
2025-08-29 20:44:50 -1000

    i don't know much about firewall OMEgALUL

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -2,34 +2,12 @@
   networking = {
     hostName = "${host}";
     networkmanager.enable = true;
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [
-        22
-        80
-        443
-        59010
-        59011
-      ];
-      allowedUDPPorts = [
-        59010
-        59011
-      ];
-      # allowedUDPPortRanges = [
-      # { from = 4000; to = 4007; }
-      # { from = 8000; to = 8010; }
-      # ];
-    };
+    firewall.enable = true;
   };
 
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
-  };
-
-  services.resolved = {
-    enable = true;
-    dnsovertls = "true";
   };
 }
